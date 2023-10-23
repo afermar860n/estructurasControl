5d52184ed405a13a924275e6015fe5f931ec6928
afermar860n <146812421+afermar860n@users.noreply.github.com>
2023-10-23 15:08:46 +0200

    EstructurasControl

--- src/ContandoNumeros.java
+++ src/ContandoNumeros.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class ContandoNumeros {
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
         int contadorPositivos = 0;
         int contadorNegativos = 0;
-        int contadorCeros = 0;
+        int Ceros = 0;
 
         for (int i = 0; i < 10; i++) {
 
@@ -17,14 +18,14 @@ public class ContandoNumeros {
             } else if (numero < 0){
                 contadorNegativos++;
             } else {
-                contadorCeros++;
+                Ceros++;
             }
         }
 
         System.out.println(contadorPositivos + " números positivos");
-        System.out.println(contadorCeros + " números ceros");
+        System.out.println(Ceros + " ceros");
         System.out.println(contadorNegativos + " números negativos");
 
     }
-    }
 }
+
